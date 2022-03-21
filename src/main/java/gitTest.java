92f0590927fab94474e81ffffb3ecbdcbe743217
longjian <longjian@gmail.com>
2022-03-21 11:12:35 +0800

    push test

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("hello,git,changed! again by branch1");
         System.out.println("hello,git,changed! again by master code confilct ");
         System.out.println("hello,git,changed! again by branch1 code confilct");
+        System.out.println("push test");
     }
 }
