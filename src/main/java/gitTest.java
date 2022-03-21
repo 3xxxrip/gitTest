16cf48d067fef65032e5c0024fa52a859c8032fd
3xxxrip <79954233+3xxxrip@users.noreply.github.com>
2022-03-21 11:28:24 +0800

    Update gitTest.java
    pull test

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -7,5 +7,6 @@ public class gitTest {
         System.out.println("hello,git,changed! again by master code confilct ");
         System.out.println("hello,git,changed! again by branch1 code confilct");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
