5eb8502e1e36368fb3cb9fdfc47f263d3a28d55e
longjian <longjian@gmail.com>
2022-03-21 10:55:44 +0800

    fifth commit by master

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
         System.out.println("hello,git,changed!");
         System.out.println("hello,git,changed! again");
         System.out.println("hello,git,changed! again by branch1");
+        System.out.println("hello,git,changed! again by master code confilct ");
     }
 }
